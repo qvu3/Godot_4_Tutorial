94b1d1b17604bab9ed489a8c2b5d3d37562281b5
Quang Vu <astorre.vu@gmail.com>
2025-02-09 16:59:23 -0500

    created walking animations

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 @export var speed: int = 50
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "up"
+		if velocity.x < 0: direction = "left"
+		elif velocity.x > 0: direction = "right"
+		elif velocity.y > 0: direction = "down"
+		
+		animations.play("Walk_" + direction)
 	
 func _physics_process(delta: float) -> void:
 	handleInput()
 	move_and_slide()
+	updateAnimation()
